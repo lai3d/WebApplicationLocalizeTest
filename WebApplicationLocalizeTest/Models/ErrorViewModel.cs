cfb9ca0563c0b398fb9617632dfd4b67b086c106
Larry Lai <larry.l@bidot.sg>
2022-08-25 09:53:21 +0800

    Add project files.

---
+++ WebApplicationLocalizeTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationLocalizeTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
