185c4f256fcae9846677240ff62e50be9ed862e9
Larry Lai <larry.l@bidot.sg>
2022-08-26 18:06:53 +0800

    test i18n in js

--- WebApplicationLocalizeTest/wwwroot/js/site.js
+++ WebApplicationLocalizeTest/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const banana = new Banana();
+banana.setLocale("zh");
+
+fetch('/i18n/demo-' + banana.locale + '.json').then((response) => response.json()).then((messages) => {
+    banana.load(messages, banana.locale)
+    console.log('i18n json loaded, test:', banana.i18n('Category'))
+})
